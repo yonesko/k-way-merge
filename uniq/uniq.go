d5785b9daf83e2e8d530b62fbcb1ec3add44f3e9
gdanichev <GDanichev@artenecy.ru>
2023-06-23 15:42:11 +0300

    uniq.go

--- uniq/uniq.go
+++ uniq/uniq.go
@@ -1,8 +1,39 @@
 package uniq
 
-import "io"
+import (
+	"bufio"
+	"fmt"
+	"io"
+)
 
 func Uniq(input io.Reader, output io.Writer) error {
+	scanner := bufio.NewScanner(input)
+	count := 0
+	prevText := ""
+	for scanner.Scan() {
+		text := scanner.Text()
+		if prevText == "" {
+			prevText = text
+		}
+		if text != prevText {
+			_, err := output.Write([]byte(fmt.Sprintf("%s %d\n", prevText, count)))
+			if err != nil {
+				return fmt.Errorf("uniq Write %w", scanner.Err())
+			}
+			prevText = text
+			count = 0
+		}
+		count++
+	}
+	if scanner.Err() != nil {
+		return fmt.Errorf("uniq scanner.Err() %w", scanner.Err())
+	}
+	if prevText != "" {
+		_, err := output.Write([]byte(fmt.Sprintf("%s %d\n", prevText, count)))
+		if err != nil {
+			return fmt.Errorf("uniq Write %w", scanner.Err())
+		}
+	}
 	return nil
 
 }
---
+++ uniq/uniq_test.go
@@ -0,0 +1,64 @@
+package uniq
+
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"strings"
+	"testing"
+)
+
+func TestUniq(t *testing.T) {
+	t.Run("regular", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		err := Uniq(strings.NewReader(`asd
+asd
+end
+end
+is
+sad
+test
+the
+this
+this
+`), buffer)
+		require.NoError(t, err)
+		assert.Equal(t, `asd 2
+end 2
+is 1
+sad 1
+test 1
+the 1
+this 2
+`, buffer.String())
+
+	})
+	t.Run("one", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		err := Uniq(strings.NewReader(`asd`), buffer)
+		require.NoError(t, err)
+		assert.Equal(t, `asd 1
+`, buffer.String())
+
+	})
+	t.Run("one many times", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		err := Uniq(strings.NewReader(`asd
+asd
+asd
+asd
+asd
+`), buffer)
+		require.NoError(t, err)
+		assert.Equal(t, `asd 5
+`, buffer.String())
+
+	})
+	t.Run("empty", func(t *testing.T) {
+		buffer := &bytes.Buffer{}
+		err := Uniq(strings.NewReader(``), buffer)
+		require.NoError(t, err)
+		assert.Equal(t, ``, buffer.String())
+
+	})
+}
